6e7c44aac42900d019cf48b4b9ffeca7bce452a9
Corbin Garner <Corbingar@gmail.com>
2021-04-22 20:10:39 -0700

    V0.1 Added Basics

--- server.js
+++ server.js
@@ -8,3 +8,21 @@ const uniqid = require('uniqid');
 // sets up app and ports
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
+app.get('/api/notes', (req, res) => {
+
+});
+
+
+app.post('/api/notes', (req, res) => {
+
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+
+});
+
+
+// Starts the server to begin listening
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
